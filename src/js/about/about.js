a3d83e972886ada3833465f570bb7958a27ba110
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-03-11 14:08:31 +0100

    :help recovery

--- src/js/about/about.js
+++ src/js/about/about.js
@@ -1,1 +1,10 @@
 import Accordion from 'accordion-js';
+
+new Accordion('.about-description-list', {
+  duration: 500,
+  showMultiple: true,
+  elementClass: 'about-item',
+  triggerClass: 'about-title',
+  panelClass: 'about-description',
+  activeClass: 'is-active',
+});
