b586167c583b5adf9ee6abafa00f24e53634e749
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-03-11 16:02:19 +0200

    swiper is not working

--- src/js/reviews/reviews.js
+++ src/js/reviews/reviews.js
@@ -1,41 +1,62 @@
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-// import Swiper JS
+// import Swiper
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper styles
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+
+const gallery = document.querySelector('.reviews-gallery');
 
 getAllFoto();
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Keyboard, Mousewheel],
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  direction: 'horizontal',
+  slidesPerView: 1,
+  grabCursor: true,
+  simulateTouch: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    sensitivity: 1,
+  },
+  speed: 400,
+});
 
 async function getAllFoto() {
   try {
     const result = await axios.get(
       `https://portfolio-js.b.goit.study/api/reviews`
     );
+    console.log(result.data);
     makeGallery(result.data);
 
-    const targetLast = document.querySelector('.swiper-slide:last-child');
-    const targetFirst = document.querySelector('.swiper-slide:first-child');
+    // const targetLast = document.querySelector('.swiper-slide:last-child');
+    // const targetFirst = document.querySelector('.swiper-slide:first-child');
 
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-    };
+    // const options = {
+    //   rootMargin: '0px',
+    //   threshold: 1.0,
+    // };
 
-    const observerLast = new IntersectionObserver(arr => {
-      if (arr[0].isIntersecting) console.log('LAST', arr[0]);
-    }, options);
+    // const observerLast = new IntersectionObserver(arr => {
+    //   if (arr[0].isIntersecting) console.log('LAST', arr[0]);
+    // }, options);
 
-    const observerFirst = new IntersectionObserver(arr => {
-      if (arr[0].isIntersecting) console.log('FIRST', arr[0]);
-    }, options);
+    // const observerFirst = new IntersectionObserver(arr => {
+    //   if (arr[0].isIntersecting) console.log('FIRST', arr[0]);
+    // }, options);
 
-    observerFirst.observe(targetFirst);
-    observerLast.observe(targetLast);
+    // observerFirst.observe(targetFirst);
+    // observerLast.observe(targetLast);
   } catch (error) {
     const messageErr = `<li class="reviews-item">
         <div class="reviews-content">
@@ -78,11 +99,11 @@ function messageAllert(message, text, backColor) {
 
 function makeGallery(array) {
   // =========================================== gallery
-  const gallery = document.querySelector('.reviews-gallery');
   const listImages = array
     .map(
       element =>
         `<li class="swiper-slide">
+      <div class="slide-container">
           <div class="reviews-thumb">
             <img
               src="${element.avatar_url}"
@@ -96,6 +117,7 @@ function makeGallery(array) {
             <p class="reviews-text">${element.review}
             </p>
           </div>
+          </div>
         </li>
     `
     )
@@ -103,40 +125,18 @@ function makeGallery(array) {
   gallery.insertAdjacentHTML('beforeend', listImages);
 
   // =========================================== buttons
-  const reviewsCards = document.querySelector('.swiper');
-  const listBtn = `<div class="swiper-buttons">
-      <div class="swiper-btn btn-left">
-        <svg class="arrow">
-          <use href="./img/sprite.svg#icon-arrow-narrow-right"></use>
-        </svg>
-      </div>
-      <div class="swiper-btn btn-right">
-        <svg class="arrow">
-          <use href="./img/sprite.svg#icon-arrow-narrow-right"></use>
-        </svg>
-      </div>
-    </div>`;
-  reviewsCards.insertAdjacentHTML('beforeend', listBtn);
-
-  const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-
-    cssMode: true,
-    navigation: {
-      nextEl: '.btn-right',
-      prevEl: '.btn-left',
-      keyboard: true,
-    },
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-    // navigation: {
-    //   nextEl: '.swiper-right',
-    //   prevEl: '.swiper-left',
-    // },
-    // mousewheel: {
-    //   invert: true,
-    // },
-    // keyboard: true,
-  });
+  // const reviewsCards = document.querySelector('.swiper');
+  // const listBtn = `<div class="swiper-buttons">
+  //       <button type="button" class="swiper-btn swiper-button-prev rotate">
+  //         <svg class="arrow" width="25" height="24">
+  //           <use href="./img/sprite.svg#icon-arrow-narrow-right"></use>
+  //         </svg>
+  //       </button>
+  //       <button type="button" class="swiper-btn swiper-button-next">
+  //         <svg class="arrow" width="25" height="24">
+  //           <use href="./img/sprite.svg#icon-arrow-narrow-right"></use>
+  //         </svg>
+  //       </button>
+  //     </div>`;
+  // reviewsCards.insertAdjacentHTML('beforeend', listBtn);
 }
