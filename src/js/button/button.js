c4033ec103ca64af68030f6bf169376073252f89
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-03-13 16:59:05 +0200

    button-return added

---
+++ src/js/button/button.js
@@ -0,0 +1,16 @@
+const target = document.querySelector('.hero');
+const btnReturn = document.getElementById('btn-return');
+const options = {
+  rootMargin: '0px',
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(arr => {
+  if (arr[0].isIntersecting) {
+    btnReturn.classList.add('visually-hidden');
+  } else {
+    btnReturn.classList.remove('visually-hidden');
+  }
+}, options);
+
+observer.observe(target);
